76045eee6f25d3ebcfa791b5269b0ad7d756e684
ling <linghuajie2019@gmail.com>
2020-02-10 16:30:03 +0800

    webpack plugin的使用

---
+++ 前端模块化开发/webpack的使用/05-webpack使用plugin/src/js/info.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion: ES6模块化开发
+ * @Author: ling
+ * @Date: 2019-11-26 16:36:59
+ * @LastEditors: ling
+ * @LastEditTime: 2019-11-26 16:38:14
+ */
+export const name = 'andy'
+export const age = 18
+export const height = 2.0
---
+++ 前端模块化开发/webpack的使用/05-webpack使用plugin/src/vue/Cpn.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="title">{{name}}</div>
+</template>
+
+<script>
+  export default {
+    name: "Cpn",
+    data(){
+      return{
+        name:'Cpn组件标题'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
